83f637cbb7bf90e7996672115da1fe92b12e7f75
fbasios <fbasios@admin.grnet.gr>
2023-08-25 13:22:37 +0300

    List of published assessments per type and actor
    This commit adds a new endpoint that provides published assessments categorized by type and actor.
    The endpoint `v1/assessments/by-type/{type-id}/by-actor/{actor-id}` allows logged-in users to retrieve all
    published assessments.

--- repository/src/main/java/org/grnet/cat/repositories/AssessmentRepository.java
+++ repository/src/main/java/org/grnet/cat/repositories/AssessmentRepository.java
@@ -1,10 +1,13 @@
 package org.grnet.cat.repositories;
 
+import io.quarkus.hibernate.orm.panache.Panache;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.grnet.cat.entities.Assessment;
 import org.grnet.cat.entities.Page;
 import org.grnet.cat.entities.PageQuery;
 import org.grnet.cat.entities.PageQueryImpl;
+
+import java.util.List;
 
 @ApplicationScoped
 
@@ -27,6 +30,43 @@ public class AssessmentRepository implements Repository<Assessment, Long> {
         pageable.index = page;
         pageable.size = size;
         pageable.count = panache.count();
+        pageable.page = Page.of(page, size);
+
+        return pageable;
+    }
+
+    /**
+     * Retrieves a page of published assessments categorized by type and actor, created by all users.
+     *
+     * @param page The index of the page to retrieve (starting from 0).
+     * @param size The maximum number of assessments to include in a page.
+     * @param typeId The ID of the Assessment Type.
+     * @param actorId The Actor's id.
+     * @return A list of Assessment objects representing the assessments in the requested page.
+     */
+    @SuppressWarnings("unchecked")
+    public PageQuery<Assessment> fetchPublishedAssessmentsByTypeAndActorAndPage(int page, int size, Long typeId, Long actorId){
+
+        var em = Panache.getEntityManager();
+
+        var query =  em.createNativeQuery("SELECT a.id, a.assessment_doc, a.template_id, a.validation_id, a.created_on, a.updated_on FROM Assessment a INNER JOIN Template t ON a.template_id = t.id INNER JOIN Actor actor ON t.actor_id = actor.id INNER JOIN AssessmentType at ON t.assessment_type_id = at.id WHERE actor.id = :actorId AND at.id = :typeId AND JSON_EXTRACT(a.assessment_doc, '$.published') = true", Assessment.class)
+                .setParameter("actorId", actorId)
+                .setParameter("typeId", typeId);
+
+        var countQuery =  em.createNativeQuery("SELECT count(a.id) FROM Assessment a INNER JOIN Template t ON a.template_id = t.id INNER JOIN Actor actor ON t.actor_id = actor.id INNER JOIN AssessmentType at ON t.assessment_type_id = at.id WHERE actor.id = :actorId AND at.id = :typeId AND JSON_EXTRACT(a.assessment_doc, '$.published') = true", Long.class)
+                .setParameter("actorId", actorId)
+                .setParameter("typeId", typeId);
+
+        var list = (List<Assessment>) query
+                .setFirstResult(page * size)
+                .setMaxResults(size)
+                .getResultList();
+
+        var pageable = new PageQueryImpl<Assessment>();
+        pageable.list = list;
+        pageable.index = page;
+        pageable.size = size;
+        pageable.count = (Long) countQuery.getSingleResult();
         pageable.page = Page.of(page, size);
 
         return pageable;
