586277fa40eb2f5bf19f5ff0578ffc325389170d
cthermolia-grnet <77612126+cthermolia-grnet@users.noreply.github.com>
2023-09-18 12:46:20 +0300

    Revert "use INTEGER insteadof TINYINT (#85)"
    This reverts commit 9c67b73793b656db067504a559c98f3c2f0fac77.

--- entity/src/main/resources/db/migration/V1.34__update_validation_status.sql
+++
@@ -1,7 +0,0 @@
--- ------------------------------------------------
--- Version: v1.34
---
--- Description: Migration that updates the validation status column in the Validation table
--- -------------------------------------------------
-
-ALTER TABLE Validation MODIFY status INTEGER NOT NULL;
