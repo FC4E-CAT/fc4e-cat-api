c129ca0d57bc98276c619fdfae749d06fd9087be
cthermolia-grnet <cthermolia@admin.grnet.gr>
2024-02-23 15:59:17 +0200

    problem during streaming mails

--- service/src/main/java/org/grnet/cat/services/MailerService.java
+++ service/src/main/java/org/grnet/cat/services/MailerService.java
@@ -11,6 +11,7 @@ import org.grnet.cat.enums.MailType;
 import org.grnet.cat.repositories.KeycloakAdminRepository;
 import org.grnet.cat.repositories.UserRepository;
 import org.jboss.logging.Logger;
+import org.keycloak.representations.idm.UserRepresentation;
 
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
@@ -42,24 +43,36 @@ public class MailerService {
     public List<String> retrieveAdminEmails() {
 
         LOG.info("Retrieve emails:");
-        LOG.info("Retrieve emails Active Threads:"+Thread.activeCount());
+        LOG.info("Retrieve emails Active Threads:" + Thread.activeCount());
         ArrayList<String> vops = new ArrayList<>();
         var admins = keycloakAdminRepository.fetchRolesMembers("admin");
         LOG.info("Fetch admins successful ");
-        admins.stream().map(admin -> admin.getAttributes().get("voperson_id")).forEach(vops::addAll);
-        return vops.stream().map(person -> userRepository.fetchUser(person).getEmail()).filter(Objects::nonNull).collect(Collectors.toList());
+        List<String> emails = new ArrayList<>();
+        for (UserRepresentation ur : admins) {
+            List<String> voips = ur.getAttributes().get("voperson_id");
+            for (String voip : voips) {
+                LOG.info("VOIP: " + voip);
+                var user = userRepository.fetchUser(voip);
+                if (user.getEmail() != null) {
+                    emails.add(user.getEmail());
+                }
+            }
+        }
+        return emails;
+        //        admins.stream().map(admin -> admin.getAttributes().get("voperson_id")).forEach(vops::addAll);
+        //      return vops.stream().map(person -> userRepository.fetchUser(person).getEmail()).filter(Objects::nonNull).collect(Collectors.toList());
     }
 
     public void sendMails(Validation val, MailType type, List<String> mailAddrs) {
-        LOG.info("SEND MAILS - CURRENT THREAD "+Thread.currentThread().getName());
-        LOG.info("Send emails Active Threads:"+Thread.activeCount());
+        LOG.info("SEND MAILS - CURRENT THREAD " + Thread.currentThread().getName());
+        LOG.info("Send emails Active Threads:" + Thread.activeCount());
         switch (type) {
             case ADMIN_ALERT_NEW_VALIDATION:
-                LOG.info("Notify Admins: "+ Arrays.toString(mailAddrs.toArray())+" for validation "+val.getId());
+                LOG.info("Notify Admins: " + Arrays.toString(mailAddrs.toArray()) + " for validation " + val.getId());
                 notifyAdmins(val, mailAddrs);
                 break;
             case VALIDATED_ALERT_CHANGE_VALIDATION_STATUS:
-                LOG.info("Notify User: "+ val.getUser().getEmail()+" for validation "+val.getId());
+                LOG.info("Notify User: " + val.getUser().getEmail() + " for validation " + val.getId());
 
                 var addrs = new ArrayList<String>();
                 addrs.add(val.getUser().getEmail());
@@ -88,11 +101,11 @@ public class MailerService {
         templateParams.put("status", validation.getStatus().name());
 
         var mail = buildEmail(templateParams, MailType.VALIDATED_ALERT_CHANGE_VALIDATION_STATUS, String.valueOf(Arrays.asList(mailAddrs)));
-        try{
+        try {
             mailer.send(mail);
-        } catch (Exception e){
+        } catch (Exception e) {
 
-            LOG.error("Cannot send the email because of : "+e.getMessage());
+            LOG.error("Cannot send the email because of : " + e.getMessage());
         }
     }
 
@@ -102,14 +115,15 @@ public class MailerService {
         templateParams.put("valUrl", uiBaseUrl + "/admin/validations/" + validation.getId());
 
         var mail = buildEmail(templateParams, MailType.ADMIN_ALERT_NEW_VALIDATION, String.valueOf(Arrays.asList(mailAddrs)));
-        try{
-            LOG.info("EMAIL INFO "+"from: "+mail.getFrom()+" to: "+Arrays.toString(mail.getTo().toArray())+" subject: "+mail.getSubject()+" message:"+mail.getText());
+        try {
+            LOG.info("EMAIL INFO " + "from: " + mail.getFrom() + " to: " + Arrays.toString(mail.getTo().toArray()) + " subject: " + mail.getSubject() + " message:" + mail.getText());
             mailer.send(mail);
-        } catch (Exception e){
+        } catch (Exception e) {
 
-            LOG.error("Cannot send the email because of : "+e.getMessage());
+            LOG.error("Cannot send the email because of : " + e.getMessage());
         }
     }
+
     public static class CustomCompletableFuture<T> extends CompletableFuture<T> {
         static final Executor EXEC = Executors.newCachedThreadPool();
 
@@ -127,17 +141,17 @@ public class MailerService {
 
         public static CompletableFuture<Void> runAsync(Runnable runnable) {
             return supplyAsync(() -> {
-                LOG.info("Running on thread: "+Thread.currentThread().getName());
+                LOG.info("Running on thread: " + Thread.currentThread().getName());
                 runnable.run();
                 return null;
             });
         }
 
         public static <U> CompletableFuture<U> supplyAsync(Supplier<U> supplier) {
-           LOG.info("Syncing on thread: "+Thread.currentThread().getName());
-           LOG.info("Active Threads: "+Thread.activeCount());
+            LOG.info("Syncing on thread: " + Thread.currentThread().getName());
+            LOG.info("Active Threads: " + Thread.activeCount());
 
-           return new CompletableFuture<U>().completeAsync(supplier);
+            return new CompletableFuture<U>().completeAsync(supplier);
         }
     }
 
