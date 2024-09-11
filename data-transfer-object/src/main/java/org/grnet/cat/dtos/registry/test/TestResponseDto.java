e6e5ecfef216d124366b7488042445ae1527fa5c
Irina Skopouli <irina@grnet.gr>
2024-09-11 14:05:21 +0300

    CAT-483 Metrics & Tests

---
+++ data-transfer-object/src/main/java/org/grnet/cat/dtos/registry/test/TestResponseDto.java
@@ -0,0 +1,77 @@
+package org.grnet.cat.dtos.registry.test;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import java.sql.Timestamp;
+
+public class TestResponseDto {
+
+    @Schema(
+            type = SchemaType.STRING,
+            implementation = String.class,
+            description = "The unique identifier for the metric",
+            example = "pid_graph:9F1A6267"
+    )
+    @JsonProperty("id")
+    public String id;
+
+    @Schema(
+            type = SchemaType.STRING,
+            implementation = String.class,
+            description = "The Test Name",
+            required = true,
+            example = "T12"
+    )
+    @JsonProperty("tes")
+    public String TES;
+
+    @Schema(
+            type = SchemaType.STRING,
+            implementation = String.class,
+            description = "Test label",
+            example = "PID Persistence - Service - Evidence")
+    @JsonProperty("label")
+    public String labelTest;
+
+    @Schema(
+            type = SchemaType.STRING,
+            implementation = String.class,
+            description = "Test description",
+            example = "Evidence is provided by the service that PIDs cannot be deleted.")
+    @JsonProperty("description")
+    public String descTest;
+
+    @Schema(
+            type = SchemaType.STRING,
+            implementation = String.class,
+            description = "Motivation ID",
+            example = "pid_graph:3E109BBA")
+    @JsonProperty("lodmtv")
+    public String lodMTV;
+
+    @Schema(
+            type = SchemaType.STRING,
+            implementation = String.class,
+            description = "Populated by",
+            example = "0000-0002-0255-5101")
+
+    @JsonProperty("populated_by")
+    public String populatedBy;
+
+    @Schema(
+            type = SchemaType.STRING,
+            implementation = String.class,
+            description = "Last modification time",
+            example = "2024-08-22T14:34:00"
+    )
+    @JsonProperty("last_touch")
+    public Timestamp lastTouch;
+
+    @Schema(
+            type = SchemaType.STRING,
+            description = "Test version")
+    @JsonProperty("lodtes_v")
+    public String lodTES_V;
+}
---
+++ service/src/main/java/org/grnet/cat/services/registry/TestService.java
@@ -0,0 +1,103 @@
+package org.grnet.cat.services.registry;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.core.UriInfo;
+import org.grnet.cat.dtos.pagination.PageResource;
+import org.grnet.cat.dtos.registry.test.TestRequestDto;
+import org.grnet.cat.dtos.registry.test.TestResponseDto;
+import org.grnet.cat.dtos.registry.test.TestUpdateDto;
+import org.grnet.cat.mappers.registry.TestMapper;
+import org.grnet.cat.repositories.registry.TestRepository;
+
+import org.jboss.logging.Logger;
+
+import java.util.Objects;
+
+@ApplicationScoped
+public class TestService {
+
+    @Inject
+    TestRepository testRepository;
+
+    private static final Logger LOG = Logger.getLogger(TestService.class);
+
+    /**
+     * Retrieves a specific Test item by its ID.
+     *
+     * @param id The unique ID of the Test item.
+     * @return The corresponding Test DTO.
+     */
+    public TestResponseDto getTestById(String id) {
+
+        var test = testRepository.findById(id);
+
+        return TestMapper.INSTANCE.testToDto(test);
+    }
+
+    /**
+     * Creates a new Test item.
+     *
+     * @param userId The user creating the Test.
+     * @param testRequestDto The Test request data.
+     * @return The created Test DTO.
+     */
+    @Transactional
+    public TestResponseDto createTest(String userId, TestRequestDto testRequestDto) {
+
+        var test = TestMapper.INSTANCE.testToEntity(testRequestDto);
+
+        test.setPopulatedBy(userId);
+
+        testRepository.persist(test);
+
+        return TestMapper.INSTANCE.testToDto(test);
+    }
+
+    /**
+     * Updates an existing Test item.
+     *
+     * @param id The unique ID of the Test to update.
+     * @param userId The user performing the update.
+     * @param request The Test update data.
+     * @return The updated Test DTO.
+     */
+    @Transactional
+    public TestResponseDto updateTest(String id, String userId, TestUpdateDto request) {
+
+        var test = testRepository.findById(id);
+
+        TestMapper.INSTANCE.updateTestFromDto(request, test);
+        test.setPopulatedBy(userId);
+
+        return TestMapper.INSTANCE.testToDto(test);
+    }
+
+    /**
+     * Deletes a Test item by its ID.
+     *
+     * @param id The unique ID of the Test item.
+     */
+    @Transactional
+    public boolean deleteTest(String id) {
+
+        return testRepository.deleteById(id);
+    }
+
+    /**
+     * Retrieves a page of Test items.
+     *
+     * @param page The index of the page to retrieve (starting from 0).
+     * @param size The maximum number of Test items to include in a page.
+     * @param uriInfo The Uri Info for pagination links.
+     * @return A PageResource containing the Test items in the requested page.
+     */
+    public PageResource<TestResponseDto> getTestlistAll(int page, int size, UriInfo uriInfo) {
+
+        var testPage = testRepository.fetchTestByPage(page, size);
+        var testDtos = TestMapper.INSTANCE.testToDtos(testPage.list());
+
+        return new PageResource<>(testPage, testDtos, uriInfo);
+    }
+}
