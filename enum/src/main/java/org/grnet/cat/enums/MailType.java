6b9faa83774e3c9cc04b345129d3f2a7826a2b80
Irina Skopouli <irina@grnet.gr>
2024-10-04 18:05:22 +0300

    CAT-543 Implement Email Notification for User on Shared Assessment

--- enum/src/main/java/org/grnet/cat/enums/MailType.java
+++ enum/src/main/java/org/grnet/cat/enums/MailType.java
@@ -87,6 +87,34 @@ public enum MailType {
             return new MailTemplate("["+templateParams.get("title")+"] - Validation Request Created with id: "+templateParams.get("valId"), body);
         }
 
+    },
+    USER_ALERT_SHARED_ASSESSMENT {
+        public MailTemplate execute(Template emailTemplate, HashMap<String, String> templateParams) {
+
+            URL url;
+            String urlString = templateParams.get("assessmentUrl");
+            try {
+                url = new URL(urlString);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+
+            String body = emailTemplate.data("urlpath", url.toString())
+                    .data("assessmentUrl", templateParams.get("assessmentUrl"))
+                    .data("assessmentName", templateParams.get("assessmentName"))
+                    .data("assessmentId", templateParams.get("assessmentId"))
+                    .data("image", templateParams.get("image"))
+                    .data("image1", templateParams.get("image1"))
+                    .data("image2", templateParams.get("image2"))
+                    .data("image3", templateParams.get("image3"))
+                    .data("image4", templateParams.get("image4"))
+                    .data("cat", templateParams.get("cat"))
+                    .data("userrole", templateParams.get("userrole"))
+                    .data("contactMail", templateParams.get("contactMail"))
+                    .render();
+
+            return new MailTemplate("[" + templateParams.get("title") + "] - Shared Assessment with name: " + templateParams.get("assessmentName"), body);
+        }
     };
 
     public abstract MailTemplate execute(Template mailTemplate, HashMap<String, String> templateParams);
