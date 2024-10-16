9039b63c145b89ff0460398ad1818e1ab3b498fa
fbasios <fbasios@admin.grnet.gr>
2024-10-16 15:37:41 +0300

    Fix Criteria fetching and sorting

--- mapper/src/main/java/org/grnet/cat/mappers/registry/PrincipleCriterionMapper.java
+++ mapper/src/main/java/org/grnet/cat/mappers/registry/PrincipleCriterionMapper.java
@@ -14,6 +14,7 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -44,9 +45,11 @@ public interface PrincipleCriterionMapper {
 
     @Named("mapPrinciples")
     default List<PrinciplePartialResponse> mapPrinciples(Set<PrincipleCriterionJunction> principles) {
+
         return principles.stream()
                 .map(this::mapPrinciple)
                 .distinct()
+                .sorted(Comparator.comparing(e -> Integer.parseInt(e.pri.substring(1))))
                 .collect(Collectors.toList());
     }
 
@@ -54,7 +57,6 @@ public interface PrincipleCriterionMapper {
     @Mapping(target = "id",  expression = "java(principleJunction.getPrinciple().getId())")  // Ignore the id field here as well
     @Mapping(target = "pri", expression = "java(principleJunction.getPrinciple().getPri())")
     @Mapping(target = "label", expression = "java(principleJunction.getPrinciple().getLabel())")
-
     PrinciplePartialResponse mapPrinciple(PrincipleCriterionJunction principleJunction);
 
     @Mapping(target = "principleId", expression = "java(principleCriterion.getPrinciple().getId())")
