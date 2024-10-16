bfc0fd00db37d7ea7dc3b27a74ce4ab28825143c
cthermolia-grnet <cthermolia@admin.grnet.gr>
2024-10-16 13:50:38 +0300

    update principle

---
+++ entity/src/main/resources/db/migration/tables/registry/V1.129__update_principle_new_motivation.sql
@@ -0,0 +1,4 @@
+update p_Principle set descPrinciple='Ensuring site security controls are in place and maintained to meet the security requirements of the hosted Communities' where lodPRI='pid_graph:5PR0882I';
+
+
+
