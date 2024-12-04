a27828e7e458a1e1b5d331c7274735c54a14e272
cthermolia-grnet <cthermolia@admin.grnet.gr>
2024-12-04 13:21:05 +0200

    delete principle-criteria

---
+++ entity/src/main/resources/db/migration/tables/registry/V1.46__delete_priniciple_criterion_from_eosc_pid_policy.sql
@@ -0,0 +1,44 @@
+-- ------------------------------------------------
+-- Version: v1.46
+--
+-- Description: Migration that deletes Principle-Criteria relations  from the p_Principle table for eosc pid policy
+-- -------------------------------------------------
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:704DEDC5' AND criterion_lodCRI = 'pid_graph:A8EA1C61' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:AE92131B' AND criterion_lodCRI = 'pid_graph:A8EA1C61' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:E533DD9B' AND criterion_lodCRI = 'pid_graph:4ED69013' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:6DA48AC0' AND criterion_lodCRI = 'pid_graph:3BB55CAF' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:215EFC78' AND criterion_lodCRI = 'pid_graph:10E055C4' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:F9141635' AND criterion_lodCRI = 'pid_graph:59DE77D5' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:F9141635' AND criterion_lodCRI = 'pid_graph:28D9C36B' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:704DEDC5' AND criterion_lodCRI = 'pid_graph:5F81AEA8' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:704DEDC5' AND criterion_lodCRI = 'pid_graph:F9FC6BE3' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:6279AE43' AND criterion_lodCRI = 'pid_graph:D969810C' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:E7C00DBA' AND criterion_lodCRI = 'pid_graph:D969810C' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:C6842E03' AND criterion_lodCRI = 'pid_graph:134EA685' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:E7C00DBA' AND criterion_lodCRI = 'pid_graph:134EA685' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:704DEDC5' AND criterion_lodCRI = 'pid_graph:134EA685' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:333F1C6A' AND criterion_lodCRI = 'pid_graph:D7C00C05' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:704DEDC5' AND criterion_lodCRI = 'pid_graph:D7C00C05' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:6DA48AC0' AND criterion_lodCRI = 'pid_graph:DF512939' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:6DA48AC0' AND criterion_lodCRI = 'pid_graph:317587D0' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:6DA48AC0' AND criterion_lodCRI = 'pid_graph:7FA9929C' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:6F00A944' AND criterion_lodCRI = 'pid_graph:5EA1B0C5' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:6890514A' AND criterion_lodCRI = 'pid_graph:27346307' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:F9141635' AND criterion_lodCRI = 'pid_graph:27346307' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:AE92131B' AND criterion_lodCRI = 'pid_graph:954437E3' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:704DEDC5' AND criterion_lodCRI = 'pid_graph:024DEF14' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:704DEDC5' AND criterion_lodCRI = 'pid_graph:D0339C6A' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:869E5028' AND criterion_lodCRI = 'pid_graph:D0339C6A' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:AE92131B' AND criterion_lodCRI = 'pid_graph:A5A41F03' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:F9141635' AND criterion_lodCRI = 'pid_graph:135BBF9C' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:AE92131B' AND criterion_lodCRI = 'pid_graph:135BBF9C' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:C6842E03' AND criterion_lodCRI = 'pid_graph:135BBF9C' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:E7C00DBA' AND criterion_lodCRI = 'pid_graph:8503588B' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:6F00A944' AND criterion_lodCRI = 'pid_graph:8503588B' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:AE92131B' AND criterion_lodCRI = 'pid_graph:213337DB' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:6DA48AC0' AND criterion_lodCRI = 'pid_graph:3DBBBFBF' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:704DEDC5' AND criterion_lodCRI = 'pid_graph:3DBBBFBF' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:704DEDC5' AND criterion_lodCRI = 'pid_graph:004D4203' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:AE92131B' AND criterion_lodCRI = 'pid_graph:694CB39D' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:6F00A944' AND criterion_lodCRI = 'pid_graph:07BEDE5D' AND lodMTV_X = 'pid_graph:3E109BBA' ;
+DELETE FROM p_Principle_Criterion WHERE principle_lodPRI = 'pid_graph:AE92131B' AND criterion_lodCRI = 'pid_graph:420EC62B' AND lodMTV_X = 'pid_graph:3E109BBA' ;
